e7f6ea3c7fa63a309643ffb429fbfbc43d2ff157
Ishan Shah <ishan0102@utexas.edu>
2023-07-05 18:48:11 -0500

    Create simple frontend with pagination

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,10 +6,12 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-function BlogPost({ title, published_at, link, description, company }) {
+const POSTS_PER_PAGE = 12;
+
+function BlogPost({ title, published_at, link, summary, company }) {
   return (
-    <Link href={link}
-      className="max-w-md mx-auto bg-white rounded-xl shadow-xl border border-gray-200 overflow-hidden md:max-w-2xl m-3 hover:bg-indigo-500 hover:text-white"
+    <Link href={link} rel="noopener noreferrer" target="_blank"
+      className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl m-3 border border-gray-200 hover:border-indigo-500 transition"
     >
       <div className="md:flex">
         <div className="p-8">
@@ -17,10 +19,13 @@ function BlogPost({ title, published_at, link, description, company }) {
             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{published_at}</div>
             <div className="text-sm">{company}</div>
           </div>
-          <div className="block mt-1 text-lg leading-tight font-medium hover:text-white">
+          <div className="block mt-1 text-lg leading-tight font-medium">
             {title}
           </div>
-          <p className="mt-2">{description}</p>
+          <p className="mt-2 text-gray-500">
+            {summary}
+            {summary.slice(-1) !== "." && "."}
+          </p>
         </div>
       </div>
     </Link>
@@ -29,24 +34,46 @@ function BlogPost({ title, published_at, link, description, company }) {
 
 export default function Home() {
   const [blogPostsList, setBlogPostsList] = useState([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     const fetchPosts = async () => {
       let { data: posts, error } = await supabase
         .from('posts')
         .select("*")
-        .order('published_at', { ascending: false });
+        .order('published_at', { ascending: false })
+        .range(page * POSTS_PER_PAGE, (page + 1) * POSTS_PER_PAGE - 1);
 
       if (error) console.error("Error fetching posts:", error);
       else setBlogPostsList(posts);
     }
 
     fetchPosts();
-  }, []);
+  }, [page]);
 
   return (
-    <div className="font-berkeley">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="font-berkeley m-8 md:m-14 pb-20">
+      <div className="flex text-center flex-col mb-4">
+        <div className="font-bold text-4xl mb-2">engblogs</div>
+        <div>learn from your favorite tech companies</div>
+      </div>
+      <div className="flex justify-center mt-6">
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page === 0}
+          className="px-3 py-2 mx-1 bg-indigo-500 text-white rounded disabled:opacity-50"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={blogPostsList.length < POSTS_PER_PAGE}
+          className="px-3 py-2 mx-1 bg-indigo-500 text-white rounded disabled:opacity-50"
+        >
+          Next
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
         {blogPostsList.map((post, index) => (
           <BlogPost
             key={post.id}
@@ -54,6 +81,7 @@ export default function Home() {
             published_at={post.published_at}
             link={post.link}
             description={post.description}
+            summary={post.summary}
             company={post.company}
           />
         ))}
