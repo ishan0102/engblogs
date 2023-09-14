86aafc18b147874863172c3530fddc9b71b913ac
Ishan Shah <ishan0102@utexas.edu>
2023-09-14 00:55:39 -0400

    Add working scripts for backfilling and post scraping

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client
 from tqdm import tqdm
-import requests
 import trafilatura
 
 # Load environment variables
@@ -11,34 +10,36 @@ url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 supabase = create_client(url, key)
 
+trafilatura.logging.getLogger().setLevel("CRITICAL")
+
 def scrape_post(link):
-    """
-    Scrape the blog post content.
-    This function is a placeholder. You would replace it with your actual scraping logic.
-    """
-    return trafilatura.html2txt(requests.get(link).text)
+    downloaded = trafilatura.fetch_url(link)
+    return trafilatura.extract(downloaded)
 
 def backfill():
-    # Fetch RSS links from the Supabase "links" table
-    response = supabase.table("links").select("company, link").execute()
-    rss_links = response.get('data', [])
-    
-    for link_data in tqdm(rss_links):
-        company = link_data['company']
-        link = link_data['link']
-        
+    response = supabase.table("posts").select("link").execute()
+    links = [link["link"] for link in response.data]
+    for link in tqdm(links):
+        # Check if the post has already been scraped
+        response = supabase.table("posts").select("full_text").eq("link", link).execute()
+        if response.data and response.data[0]["full_text"]:
+            tqdm.write(f"Skipping {link} - already scraped")
+            continue
+
         # Scrape the blog post content
-        content = scrape_post(link)
+        tqdm.write(f"Scraping {link}")
+        full_text = scrape_post(link)
+        if not full_text:
+            print(f"Trafilatura - failed to scrape {link}")
+            continue
+
+        # Update the Supabase table with the scraped content
+        response = supabase.table("posts").update({
+            "full_text": full_text,
+        }).match({"link": link}).execute()
         
-        if content:
-            # Update the Supabase table with the scraped content
-            response = supabase.table("links").update({
-                'content': content,
-            }).match({'link': link}).execute()
-            
-            if response.get('error'):
-                print(f"Failed to backfill for {link}: {response['error']}")
+        if "error" in response:
+            print(f"Supabase - failed to backfill {link}: {response['error']}")
 
 if __name__ == "__main__":
-    # backfill()
-    print(scrape_post("https://blog.janestreet.com/what-the-interns-have-wrought-2023/"))
+    backfill()
