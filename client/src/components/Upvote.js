1e8ca92ade5772f19c53eb930177100a45005a0c
Ishan Shah <ishan0102@utexas.edu>
2023-09-28 00:25:15 -0400

    Fix accidental link navigation on upvote

--- client/src/components/Upvote.js
+++ client/src/components/Upvote.js
@@ -29,7 +29,10 @@ export default function Upvote({ postId, supabase, userIP }) {
     fetchData();
   }, []);
 
-  const handleUpvote = async () => {
+  const handleUpvote = async (e) => {
+    // Don't follow the link if the user clicks the upvote button
+    e.preventDefault();
+
     let newUpvoteCount = hasUpvoted ? upvoteCount - 1 : upvoteCount + 1;
     let newHasUpvoted = !hasUpvoted;
   
@@ -61,7 +64,7 @@ export default function Upvote({ postId, supabase, userIP }) {
 
   return (
     <div className="absolute bottom-6 right-6 flex items-center">
-      <button onClick={handleUpvote}>
+      <button onClick={(e) => handleUpvote(e)}>
         {hasUpvoted ?
           (<svg className="w-5 h-6 text-indigo-500 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
             <path d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z" />
