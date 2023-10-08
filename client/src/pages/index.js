f8ef232f4e3a5e93da1fe35babafb2b2b6cbf14f
Ishan Shah <ishan0102@utexas.edu>
2023-10-07 23:51:03 -0400

    Add donation link

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -159,17 +159,32 @@ export default function Home(props) {
 
   return (
     <div className="font-berkeley m-8 md:m-10 pb-20">
+      {/* Nav Buttons */}
+      <div className="mb-12">
+        <div className="absolute top-0 left-0 md:top-4 md:left-4">
+          <a href="https://github.com/ishan0102/engblogs" target="_blank" rel="noopener noreferrer">
+            <button className="block text-sm font-apple2mono focus:outline-none relative transform transition-transform md:duration-200 md:hover:scale-105 p-2">
+              <span className="inline-block py-1 px-2 rounded-lg">
+                github
+              </span>
+            </button>
+          </a>
+        </div>
+        <div className="absolute top-0 right-0 md:top-4 md:right-4">
+          <a href="https://donate.stripe.com/9AQ4hH7TO2PrfsY4gg" target="_blank" rel="noopener noreferrer">
+            <button className="block text-white text-sm font-apple2mono focus:outline-none relative transform transition-transform md:duration-200 md:hover:scale-105 p-2">
+              <span className="inline-block py-1 px-2 rounded-lg bg-gradient-to-r from-amber-500 to-orange-600 shadow-md transition-all duration-300 ease-in-out hover:from-orange-500 hover:to-red-800">
+                donate!
+              </span>
+            </button>
+          </a>
+        </div>
+      </div>
+
       {/* Header */}
       <div className="flex text-center flex-col mb-4">
         <div className="font-bold text-4xl mb-2">engblogs</div>
         <div className="text-md">learn from your favorite tech companies</div>
-      </div>
-      <div className="absolute top-0 right-0 md:top-4 md:right-4">
-        <a href="https://github.com/ishan0102/engblogs" target="_blank" rel="noopener noreferrer">
-          <button className="max-w-md mx-auto bg-white rounded-lg text-sm border border-white hover:border-black transition p-2">
-            github
-          </button>
-        </a>
       </div>
 
       {/* Web Navigation - Shown on medium screens and up */}
